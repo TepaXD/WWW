c9f3006df23a1e3e6aa58f1cb007111103983fde
TepaXD <46355556+TepaXD@users.noreply.github.com>
2019-12-11 20:52:25 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/CustomNavbar';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/CustomNavbar";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Message from './components/Messages';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Message from "./components/Messages";
 
 //constructor to initialize state
 class App extends Component {
-	state = {
-		searchable: '',
-	};
+  state = {
+    searchable: ""
+  };
 
-	getSearchable = data => {
-		this.setState({ searchable: data });
-	};
+  getSearchable = data => {
+    this.setState({ searchable: data });
+  };
 
-	constructor(props) {
-		super(props);
-		this.state = { apiResponse: '' };
-		this.setState({ login: false });
-	}
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+    this.setState({ login: false });
+  }
 
-	render() {
-		return (
-			<Router>
-				<NavBar parentGetSearchable={this.getSearchable} />
-				<Message />
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <NavBar parentGetSearchable={this.getSearchable} />
+        <Message />
+      </Router>
+    );
+  }
 }
 
 export default App;
